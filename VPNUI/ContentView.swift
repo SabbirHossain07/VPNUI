91838de1931adb7fb57bac81b18d24fcde0e4ed4
SabbirHossain07 <sabbirsopnil07@gmail.com>
2021-09-15 18:58:04 +0600

    Initial Commit

---
+++ VPNUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VPNUI
+//
+//  Created by Sopnil Sohan on 15/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VPNUI/VPNUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPNUIApp.swift
+//  VPNUI
+//
+//  Created by Sopnil Sohan on 15/9/21.
+//
+
+import SwiftUI
+
+@main
+struct VPNUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
